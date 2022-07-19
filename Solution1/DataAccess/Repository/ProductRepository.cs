104ad37982c26f58548476971dc75e251493701b
oggy-learn-code <vunguyenhuychuong10t2@gmail.com>
2022-07-19 18:36:51 +0700

    Complete Show List Products, design Search bar and fillter

---
+++ Solution1/DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using BussinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public List<Product> GetListProducts() => ProductDAO.Instance.GetListProducts();
+
+
+        public Product GetOrderById(int id) => ProductDAO.Instance.GetProductById(id);
+
+
+        public Product GetProductById_Name(int id, string productName) 
+            => ProductDAO.Instance.GetProductsById_Name(id, productName);
+
+
+
+        public List<Product> GetProductsByUnitPrice_UnitInStock(int unitPrice, int unitslnStock)
+        => ProductDAO.Instance.GetProductsByUnitPrice_UnitInStock(unitPrice, unitslnStock);
+
+        public void Insert(Product product) => ProductDAO.Instance.InsertProduct(product);
+
+
+        public void Remove(int id) => ProductDAO.Instance.RemoveProduct(id);
+
+
+        public void Update(Product product) => ProductDAO.Instance.UpdateProduct(product);
+
+    }
+}
---
+++ Solution1/eStore/Controllers/ProductsController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BussinessObject.Models;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace eStore.Controllers
+{
+    public class ProductsController : Controller
+    {   
+        private readonly SalesManagementDBContext _context;
+
+        public ProductsController(SalesManagementDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductsController
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<IActionResult> Index(string searchString, decimal UnitPrice) 
+        {
+            var list = from p in _context.Products
+                       select p;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                list = list.Where(s => s.ProductName.Contains(searchString) && s.UnitPrice <= UnitPrice).OrderByDescending(p => p.ProductName);
+            }
+
+            return View(await list.ToListAsync());
+
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: ProductsController/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,CategoryId,ProductName,Weight,UnitPrice,UnitslnStock")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: ProductsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }//end class
+}
