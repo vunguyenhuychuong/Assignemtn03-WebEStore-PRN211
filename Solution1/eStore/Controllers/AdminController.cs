842fffdc8a836b340ba17105f0f875278e6d8c93
oggy-learn-code <vunguyenhuychuong10t2@gmail.com>
2022-07-19 11:36:09 +0700

    Complete Logout, login successfully, Show List Member

---
+++ Solution1/eStore/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using eStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace eStore.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Solution1/eStore/Controllers/LogoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eStore.Controllers
+{
+    public class LogoutController : Controller
+    {
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
