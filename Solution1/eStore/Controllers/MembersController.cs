dc8a57c9a730315c4b7a5aff7b9b1d762f323981
oggy-learn-code <vunguyenhuychuong10t2@gmail.com>
2022-07-19 12:47:01 +0700

    Delete unnessasry file

--- Solution1/eStore/Controllers/MembersController.cs
+++ Solution1/eStore/Controllers/MembersController.cs
@@ -53,7 +53,7 @@ namespace eStore.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MemberId,Email,CompanyName,City,Country,Password")] Member member)
+        public async Task<IActionResult> Create([Bind("MemberId,Email,CompanyName,City,Country,Password,RoleName")] Member member)
         {
             if (ModelState.IsValid)
             {
